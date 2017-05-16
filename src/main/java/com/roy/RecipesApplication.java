c17ce064781efbbc4bdb1fa37dcfe717cb64ce14
Roy <cinfist@gmail.com>
2017-05-17 02:06:16 +0300

    com4

--- src/main/java/com/roy/RecipesApplication.java
+++ src/main/java/com/roy/RecipesApplication.java
@@ -58,7 +58,7 @@ public class RecipesApplication extends SpringBootServletInitializer implements 
 	@Override
 	public void run(String... arg0) throws Exception {
 		System.out.println(new File("images").isDirectory() + " Is Directory?");
-		System.out.println(new File("imagess").isDirectory() + " Is Directory 2?");
+		System.out.println(new File("images").listFiles().length + " How Many Files?");
 		if ( userService.findByUsername("snakemzm") == null ) {
 			createUserWithRoles();
 		}
